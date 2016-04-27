12909cde88684c18e6de149cc1c92fa833ab6785
Kyle <kyle@eatstreet.com>
2016-04-26 21:13:42 -0500

    update sender

--- badgerloop-backend/tests/sender.go
+++ badgerloop-backend/tests/sender.go
@@ -2,39 +2,31 @@ package main
 
 import "time"
 import "github.com/exis-io/core"
-import "github.com/exis-io/core/goRiffle"
+import "github.com/exis-io/core/riffle"
 import "math/rand"
 
 func main() {
 	core.LogLevel = core.LogLevelDebug
-	var err error
-	var conn core.Connection
-	core.Fabric = "ws://192.168.99.100:8000"
 	// Create a domain
-	if conn, err = goRiffle.Open(core.Fabric); err != nil {
-		core.Error("Unable to connect")
-	} else {
-		app := core.NewDomain("xs.node", nil)
-		backend := app.Subdomain("backend")
-		datasource := app.Subdomain("datasource")
+	riffle.SetFabric("ws://192.168.99.100:8000")
 
-		// Join the fabric
-		go datasource.Join(conn)
+	// Create the domain objects
+	app := riffle.NewDomain("xs.node")
+	sender := app.Subdomain("sender")
 
+	sender.Join()
+	
 		// Super hack - sleep to let the connection come up
 		time.Sleep(5 * time.Millisecond)
 		var ep string
-		var ret []interface{}
-		ret = append(ret, rand.Intn(100))
 		ep = "demo"
 		// Do important stuff
 		for {
 			// Make the call to get the data we need
 			//core.Info("Sending %v, %v", ep, ret)
 			defer func(){main()}()
-			backend.Publish(ep, ret)
-			
+			sender.Publish(ep, rand.Intn(100))
 			time.Sleep(10 * time.Millisecond)
-		}
 	}
+	sender.Listen()
 }
