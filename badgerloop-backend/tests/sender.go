2fa71bf89d0ec381007a422b1b36bc2799de5ef1
Kyle <kyle@eatstreet.com>
2016-12-03 17:49:18 -0600

    update demo script WIP

--- badgerloop-backend/tests/sender.go
+++ badgerloop-backend/tests/sender.go
@@ -5,22 +5,30 @@ import "github.com/exis-io/core"
 import "github.com/exis-io/core/riffle"
 import "math/rand"
 
-func main() {
+var sender riffle.Domain
+var receiver riffle.Domain
+
+func connect(){
+
 	core.LogLevel = core.LogLevelDebug
 	// Create a domain
 	riffle.SetFabric("ws://192.168.1.99:8000")
-
 	// Create the domain objects
 	app := riffle.NewDomain("xs")
 	//receiver := app.Subdomain("cmd")
-	sender := app.Subdomain("node")
+	sender = app.Subdomain("node")
+	//sender.Join()
+	//return sender
+}
 
-	sender.Join()
+func main() {
+
 	
 		// Super hack - sleep to let the connection come up
 		//var ep string
 
-
+		connect()
+		sender.Join()
 		ep := "exis"
 		var m = make(map[string]int)
 		time.Sleep(200 * time.Millisecond)
