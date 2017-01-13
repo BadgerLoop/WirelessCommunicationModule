d727cf715efdc9241c635875e25e1d3b0b4482ba
Kyle <kyle@eatstreet.com>
2017-01-12 21:37:57 -0600

    try using candump for receive script

--- listener/listener.go
+++ listener/listener.go
@@ -2,68 +2,67 @@
 package main
 
 import (
-	"fmt"
-	"time"
+	// "fmt"
+	// "time"
+    "os/exec"
 	riffle "github.com/exis-io/core/riffle"
 )
 
-func reg(name string) (chan interface{}, chan interface{}) {
-	riffle.Info("Starting progressive handler")
-
-	progress, done := make(chan interface{}), make(chan interface{})
-
-	go func() {
-		for i := 1; i <= 10; i++ {
-			progress <- fmt.Sprintf("Some Progress: %d", i)
-			time.Sleep(1 * 100 * time.Millisecond)
+func listen(sender riffle.Domain){
+		for {
+			// Make the call to get the data we need
+			if out, err := exec.Command("candump can0").Output(); err != nil {
+				riffle.Error("Error %v", err)
+			} else {
+				str := string(out)
+				//if ep, pyld, err := parse(str); err == nil {
+					riffle.Info("Sending %s", out)
+					sender.Publish("can", str)
+				}
+			//}
 		}
-
-		done <- "Done!"
-	}()
-
-	return progress, done
 }
 
-func regNoProgress(name string) string {
-	riffle.Info("Receiver got message from " + name)
-	return "This is patrick"
+func cmd(command string) {
+	riffle.Info("Command received: %s", command)
 }
 
-func sub(name string) {
-	riffle.Info("Pub received! Name: %s", name)
+func hb(heartbeat string) {
+	riffle.Info("Heartbeat received: %s", heartbeat)
 }
 
 func main() {
 	// set flags for testing
 	//riffle.SetFabricLocal()
-	riffle.SetLogLevelInfo()
+	riffle.SetLogLevelDebug()
 	riffle.SetFabric("ws://192.168.1.99:9000")
 	// Domain objects
 	app := riffle.NewDomain("xs.node")
 
-	receiver := app.Subdomain("can")
+	heartbeat := app.Subdomain("hb")
+	sender := app.Subdomain("can")
+	receiver := app.Subdomain("cmd")
 	// Connect
 	//receiver.SetToken("1o1-sPF0NWy2kWcv0XHJxpVUkMHWblQrfa5-cVXcsMujjl-l3W2CNgFSR.1LIE6S-QNT31RCLWgRBvFyGFy0BznBOzvdS8Xr0z9i4iatUWDOV1EdH4PtVd4RDMA5yVr3Ioz2cdvHmWas4rA3plr8G-XiCCjzF7NYE-YYRiaOmZ0_")
 	receiver.Join()
+	heartbeat.Join()
+	sender.Join()
 
-	if e := receiver.Subscribe("cmd", sub); e != nil {
-		riffle.Info("Unable to subscribe: ", e.Error())
+	if e := receiver.Subscribe("cmd", cmd); e != nil {
+		riffle.Info("Unable to subscribe: to cmd endpoint", e.Error())
 	} else {
-		riffle.Info("Subscribed!")
+		riffle.Info("Subscribed to cmd endpoint!")
 	}
 
-	// if e := receiver.Register("reg", regNoProgress); e != nil {
-	// 	riffle.Info("Unable to register: ", e.Error())
-	// } else {
-	// 	riffle.Info("Registered!")
-	// }
+	if e := heartbeat.Subscribe("hb", hb); e != nil {
+		riffle.Info("Unable to subscribe to heartbeat endpoint ", e.Error())
+	} else {
+		riffle.Info("Subscribed to heartbeat!")
+	}
 
-	// if e := receiver.Register("progressive", reg, riffle.Options{Progress: true}); e != nil {
-	// 	riffle.Info("Unable to register: ", e.Error())
-	// } else {
-	// 	riffle.Info("Registered!")
-	// }
+	go listen(sender)
 
 	// Handle until the connection closes
+	heartbeat.Listen()
 	receiver.Listen()
 }
