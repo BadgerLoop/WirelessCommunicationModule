3709afa08523651eb92fe4f167914322ec71072b
Kyle <kyle@eatstreet.com>
2017-01-15 23:07:26 -0600

    update listener script

--- wcm/listener.go
+++ wcm/listener.go
@@ -4,25 +4,30 @@ package main
 import (
 	// "fmt"
 	//"time"
+	"strings"
     "os/exec"
 	riffle "github.com/exis-io/core/riffle"
 )
 
-func listen(sender riffle.Domain){
+func listen(app riffle.Domain){
 		for {
 			// Make the call to get the data we need
 			//if out, err := exec.Command("python3","listen.py","can0").Output(); err != nil {
-			if out, err := exec.Command("candump","can0").Output(); err != nil {
+			if out, err := exec.Command("candump","-n","1","can1").Output(); err != nil {
 				riffle.Error("Error %v", err)
 			} else {
 				str := string(out)
-				//if ep, pyld, err := parse(str); err == nil {
+				pstring := strings.Split(str, " ")
+				// if ep, pyld, err := parse(str); err == nil {
 					riffle.Info("Sending %s", out)
-					sender.Publish("can", str)
-				}
+					app.Publish("can", pstring)
+					
+					//sender.Publish("can", pstring)
 			//}
-				//time.Sleep(3 * time.Second)
+				
 		}
+		//time.Sleep(5 * time.Second)
+	}
 }
 
 func cmd(command string) {
@@ -39,32 +44,34 @@ func main() {
 	riffle.SetLogLevelInfo()
 	riffle.SetFabric("ws://192.168.1.99:9000")
 	// Domain objects
-	app := riffle.NewDomain("xs.node")
-
-	heartbeat := app.Subdomain("hb")
-	sender := app.Subdomain("can")
-	receiver := app.Subdomain("cmd")
+	core := riffle.NewDomain("xs")
+	app := core.Subdomain("node")
+	// heartbeat := app.Subdomain("hb")
+	// sender := app.Subdomain("can")
+	// receiver := app.Subdomain("cmd")
 	// Connect
 	//receiver.SetToken("1o1-sPF0NWy2kWcv0XHJxpVUkMHWblQrfa5-cVXcsMujjl-l3W2CNgFSR.1LIE6S-QNT31RCLWgRBvFyGFy0BznBOzvdS8Xr0z9i4iatUWDOV1EdH4PtVd4RDMA5yVr3Ioz2cdvHmWas4rA3plr8G-XiCCjzF7NYE-YYRiaOmZ0_")
-	receiver.Join()
-	heartbeat.Join()
-	sender.Join()
+	app.Join()
+	// heartbeat.Join()
+	// sender.Join()
 
-	if e := receiver.Subscribe("cmd", cmd); e != nil {
+	// if e := receiver.Subscribe("cmd", cmd); e != nil {
+	if e := app.Subscribe("cmd", cmd); e != nil {
 		riffle.Info("Unable to subscribe to cmd endpoint", e.Error())
 	} else {
 		riffle.Info("Subscribed to cmd endpoint!")
 	}
 
-	if e := heartbeat.Subscribe("hb", hb); e != nil {
+	// if e := heartbeat.Subscribe("hb", hb); e != nil {
+	if e := app.Subscribe("cmd", cmd); e != nil {
 		riffle.Info("Unable to subscribe to heartbeat endpoint ", e.Error())
 	} else {
 		riffle.Info("Subscribed to heartbeat!")
 	}
 
-	go listen(sender)
+	go listen(app)
 
 	// Handle until the connection closes
-	heartbeat.Listen()
-	receiver.Listen()
+	app.Listen()
+	// receiver.Listen()
 }
