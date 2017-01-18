f5ad40dfbcdcb5a79bfba3f64464bdc0786828af
Kyle <kyle@eatstreet.com>
2017-01-18 01:38:48 -0600

    add better print statement

--- misc/tests/spam_can_wcm.py
+++ misc/tests/spam_can_wcm.py
@@ -11,7 +11,7 @@ def spam():
         data = str(os.urandom(2).encode('hex'))
         raw  = sid + "#" + data 
         subprocess.call('cansend can1 '+ raw, shell=True)
-        print("Sent WCM heartbeat")
+        print("Sent WCM can message: " + raw)
         time.sleep(float(interval_ms*.001))
 
 if __name__ == '__main__':
