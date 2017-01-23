3170f9bc5ca49a7544c07fff0a73394144b36194
Kyle <kyle@eatstreet.com>
2017-01-22 16:13:57 -0800

    fix test script

--- wcm/test/run_test.py
+++ wcm/test/run_test.py
@@ -9,24 +9,31 @@ import argparse
 parser = argparse.ArgumentParser(description="A CLI for implementing String Functions over SPU ")
 parser.add_argument('-t','--test_file',default='ex_test.json',help="Location of test json file (must be in format outlined in ex_test.json.)",metavar="test_file")
 parser.add_argument('-i','--can_interface',default="can1", help="can interface used to read message from the bus",metavar="can_interface")
+parser.add_argument('-v','--validate',default=True, help="can interface used to read message from the bus",metavar="validate")
 args = vars(parser.parse_args())
 print(args)
 with open(args['test_file']) as test_file:    
     test = json.load(test_file)
 
 def validate_output(data):
+  print(data)
 	#TODO implement output validation logic
-	pass
+
 
 def run_test():
   for message in test['messages']:
     start_time = time.time() * 1000 #Time of sent can message in ms
     while ((time.time()*1000) - start_time) < message['interval_ms']:
-      	subprocess.Popen(['candump','-L','-n','1','can1'],stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-        out, err = p.communicate()
-        validate_output(out)
-
+        #Send message to CAN bus 
         subprocess.call('cansend can1 %s'%message['can'] , shell=True)
+        # Get CAN message from bus
+        p = subprocess.Popen(['candump','-L','-n','1',args['can_interface']],stdout=subprocess.PIPE)
+        out = p.stdout.readline()
+        if args['validate']:
+        # Validatate output (optional)
+          validate_output(out)
+
+
         print("Sent CAN message: %s \nWaiting %s ms" %(message['can'], message['interval_ms']))
 
 if __name__ == '__main__':
